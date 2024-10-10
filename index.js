c383b8ddb05469b76af605a8ec5ae6e078d1d7f2
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-10-10 09:54:19 +0800

    middleware added

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require("morgan");
+const cors = require('cors');
 const app = express();
 
+
+//middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server listening to port 3000')
