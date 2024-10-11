4da1eb4fbb1b1d8cd8bf9158f3f4ff4842302d15
Rajahmuden Dalaten <drajahmuden@gmail.com>
2024-10-12 04:46:13 +0800

    index modified

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require('express');
 const mongoose = require('mongoose');
+const crudRoute = require('./routes/crud.routes');
 const morgan = require("morgan");
 const cors = require('cors');
 const app = express();
+const connectDB = require('./config/db');
 
+
+//connect to DB
+connectDB();
 
 //middleware
 app.use(cors());
@@ -12,10 +17,19 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log('Server listening to port 3000')
-});
+//routes
+app.use('/api/crud', crudRoute);
+
+// app.listen(3000, () => {
+//   console.log('Server listening to port 3000')
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello, World')
+});
+
+//Start the server 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
